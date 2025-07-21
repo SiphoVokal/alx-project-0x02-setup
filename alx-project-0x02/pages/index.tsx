e9df494f4aa2b66a9ea301b1c81ef43b647f3fa5
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-07-21 14:03:07 +0200

    Set Up a Next.js Project

---
+++ alx-project-0x02/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col h-screen">
+      <main className="flex-grow flex items-center justify-center">
+        <h1 className="text-5xl font-bold">Welcome</h1>
+      </main>
+    </div>
+  );
+}
