10b798968d82e827ca08f67fde7c27005554180a
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-07-25 19:39:30 +0200

    Cred

---
+++ alx-project-0x02/components/common/Card.tsx
@@ -0,0 +1,14 @@
+import { CardProps } from "@/interfaces";
+
+const Card: React.FC<CardProps> = ({ title, content }) => {
+  return(
+    <div>
+      <div className= "rounded-lg shadow-white p-4 max-w-md py-6">
+        <h2 className="text-xl font-semibold mb-2">{title}</h2>
+        <p className="text-gray-500">{content}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,14 +1,25 @@
-import Link from "next/link";
+import Card from "../components/common/Card";
 import Header from "@/components/layout/Header";
 
 
 const Home: React.FC = () => {
     return (
-      <div className="flex flex-col h-screen">
+      <div >
         <Header />
-        <main className="flex-grow flex items-center justify-center">
-          <h1 className="text-5xl font-bold">Home</h1>
-        </main>
+        <div className="flex flex-col items-center space-y-4 p-6">
+        <Card
+          title="Welcome to Our Site"
+          content="This is the first card component with some introductory content."
+        />
+        <Card
+          title="Features"
+          content="Our platform offers great features to help you get started quickly."
+        />
+        <Card
+          title="Contact Us"
+          content="Have any questions? Reach out to our support team anytime."
+        />
+        </div>
       </div>
     );
 }
