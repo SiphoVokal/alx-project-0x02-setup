292b5aab879c1bbfe5ee213b019f5fe833b6fa59
SiphoVokal <163132466+SiphoVokal@users.noreply.github.com>
2025-07-26 15:22:27 +0200

    posts

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,40 +1,42 @@
-import React, { useEffect, useState } from "react";
-import PostCard from "../components/common/PostCard";
-import { PostProps } from "../interfaces";
+import React from "react";
+import { PostsPageProps } from "@/interfaces";
+import PostCard from "@/components/common/PostCard";
+import { PostProps } from "@/interfaces";
 import Header from "@/components/layout/Header";
 
-const Posts: React.FC = () => {
-  const [posts, setPosts] = useState<PostProps[]>([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=6")
-      .then((response) => response.json())
-      .then((data) =>
-        setPosts(
-          data.map((post: any) => ({
-            title: post.title,
-            content: post.body,
-            userId: post.userId,
-          }))
-        )
-      );
-  }, []);
-
+const Posts: React.FC<PostsPageProps> = ({ posts }) => {
   return (
     <>
-    <Header />
-    <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-      {posts.map((post, index) => (
-        <PostCard 
-          key={index}
-          title={post.title}
-          content={post.content}
-          userId={post.userId}
-        />
-      ))}
-    </div>
+      <Header />
+      <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {posts.map((post, index) => (
+          <PostCard
+            key={index}
+            title={post.title}
+            content={post.content}
+            userId={post.userId}
+          />
+        ))}
+      </div>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts?_limit=6");
+  const data = await res.json();
+
+  const posts: PostProps[] = data.map((post: any) => ({
+    title: post.title,
+    content: post.body,
+    userId: post.userId,
+  }));
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 export default Posts;
